e209dfa055ad48796d25d879edc061c40400ef8e
Chenxi Zhao <chenxizhao2008@gmail.com>
2020-09-27 00:54:29 -0600

    Added route for user name to display name

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
 
 if __name__ == '__main__':
     app.run(debug=True)
